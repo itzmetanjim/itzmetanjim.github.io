a23e5bca5eb69046507c220fb0c240eeb5202334
Tanjim Kamal <tanjimkamal1@gmail.com>
2026-04-09 16:06:08 +0000

    stuff

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-document.body.classList.toggle('dark')
+
+window.setTimeout(()=>{document.body.classList.toggle('dark')
+    document.body.classList.toggle('small')
+},100)
 window.addEventListener('keydown',(e)=>{
     if(e.key === 'p'){
         document.body.classList.toggle('dark')
